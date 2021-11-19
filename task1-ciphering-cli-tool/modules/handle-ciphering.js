029f5d161ce183403d89d3402fea3a4748a9e741
eugenemp <phoenixlp91@mail.ru>
2021-11-19 22:17:08 +0300

    feat: implement Caesar and ROT ciphering function

--- task1-ciphering-cli-tool/modules/handle-ciphering.js
+++ task1-ciphering-cli-tool/modules/handle-ciphering.js
@@ -9,6 +9,33 @@ const atbash = (str) => {
     .replace(/[A-Z]/g, (letter) => encode(letter, 65));
 };
 
+// Caesar ciphering. Also used for ROT-8
+const caesar = (type, mode) => {
+  const shift = (type === 'C' && 1) || (type === 'R' && 8);
+
+  return (str) => {
+    // Same as before this function returns encodes char...
+    const encode = (letter, min) =>
+      String.fromCharCode(((letter.charCodeAt() - min + shift) % 26) + min);
+    // ...but for Caesar we also need a decoder
+    const decode = (letter, min) => {
+      const pos = (letter.charCodeAt() - min - shift) % 26;
+      return pos < 0
+        ? String.fromCharCode(min + 26 + pos)
+        : String.fromCharCode(pos + min);
+    };
+
+    return str
+      .replace(/[a-z]/g, (letter) =>
+        mode ? encode(letter, 97) : decode(letter, 97)
+      )
+      .replace(/[A-Z]/g, (letter) =>
+        mode ? encode(letter, 65) : decode(letter, 65)
+      );
+  };
+};
+
 module.exports = {
   atbash,
+  caesar,
 };
