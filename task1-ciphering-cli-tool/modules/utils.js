2c2640175db47991beaa917d0015b6d290347f06
eugenemp <phoenixlp91@mail.ru>
2021-11-23 01:12:27 +0300

    refactor: move error catching to main file, change file validation, code cleanup

--- task1-ciphering-cli-tool/modules/utils.js
+++ task1-ciphering-cli-tool/modules/utils.js
@@ -1,44 +1,33 @@
 const fs = require('fs');
 const { FileError } = require('./error-file');
-const { handleError } = require('./handle-errors');
 
 // Check file for existence
 const checkFileExist = (path, fileType) => {
   try {
-    try {
-      fs.accessSync(path, fs.constants.F_OK);
-    } catch {
-      if (fileType) throw new FileError('ERR_INP_FILE_NOT_EXIST');
-      else throw new FileError('ERR_OUT_FILE_NOT_EXIST');
-    }
-  } catch (error) {
-    handleError(error);
+    fs.accessSync(path, fs.constants.F_OK);
+  } catch {
+    if (fileType) throw new FileError('ERR_INP_FILE_NOT_EXIST');
+    if (!fileType) throw new FileError('ERR_OUT_FILE_NOT_EXIST');
   }
 };
 
 // Check file read access
-const checkFileRead = (path) => {
+const checkFileRead = (path, fileType) => {
   try {
-    try {
-      fs.accessSync(path, fs.constants.R_OK);
-    } catch {
-      throw new FileError('ERR_INP_FILE_READ');
-    }
-  } catch (error) {
-    handleError(error);
+    fs.accessSync(path, fs.constants.R_OK);
+  } catch {
+    if (fileType) throw new FileError('ERR_INP_FILE_READ');
+    if (!fileType) throw new FileError('ERR_OUT_FILE_READ');
   }
 };
 
 // Check file read access
-const checkFileWrite = (path) => {
+const checkFileWrite = (path, fileType) => {
   try {
-    try {
-      fs.accessSync(path, fs.constants.W_OK);
-    } catch {
-      throw new FileError('ERR_OUT_FILE_WRITE');
-    }
-  } catch (error) {
-    handleError(error);
+    fs.accessSync(path, fs.constants.W_OK);
+  } catch {
+    if (fileType) throw new FileError('ERR_INP_FILE_WRITE');
+    if (!fileType) throw new FileError('ERR_OUT_FILE_WRITE');
   }
 };
 
