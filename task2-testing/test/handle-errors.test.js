34392bd5a69c00fa1b97d528ca536af67ac07376
eugenemp <phoenixlp91@mail.ru>
2021-11-24 07:30:11 +0300

    refactor: code cleanup

--- task2-testing/test/handle-errors.test.js
+++ task2-testing/test/handle-errors.test.js
@@ -1,35 +1,34 @@
 const { expect } = require('@jest/globals');
-const { FileError } = require('../modules/error-file');
 const { ValidationError } = require('../modules/error-validation');
 const { handleError } = require('../modules/handle-errors');
 
 describe('Custom error handler tests', () => {
+  const mockFns = {
+    mockExit: jest.spyOn(process, 'exit').mockImplementation(() => {}),
+    mockStderr: jest
+      .spyOn(process.stderr, 'write')
+      .mockImplementation(() => {}),
+  };
 
-  afterEach(() => {    
-    jest.clearAllMocks();
-  });
-
-  const customError = new ValidationError('ERR_WRG_ARGS');
-  const normlError = new Error('NORMAL_ERR');
+  const errors = {
+    customError: new ValidationError('ERR_WRG_ARGS'),
+    normlError: new Error('NORMAL_ERR'),
+  };
 
   test('Should call process.exit with code specified in custom error', () => {
-    const mockExit = jest.spyOn(process, 'exit').mockImplementation(() => {});
-
-    handleError(customError);
-    expect(mockExit).toHaveBeenCalledWith(1001);
+    handleError(errors.customError);
+    expect(mockFns.mockExit).toHaveBeenCalledWith(1001);
   });
 
   test('Should call process.stderr with message specified in custom error', () => {
-    const mockStderr = jest.spyOn(process.stderr, 'write').mockImplementation(() => {});
-
-    handleError(customError);
-    expect(mockStderr).toHaveBeenCalledWith('ValidationError: Wrong arguments were passed!');
+    handleError(errors.customError);
+    expect(mockFns.mockStderr).toHaveBeenCalledWith(
+      'ValidationError: Wrong arguments were passed!'
+    );
   });
 
   test('Should call process.stderr with message specified in non-custom error', () => {
-    const mockStderr = jest.spyOn(process.stderr, 'write').mockImplementation(() => {});
-
-    handleError(normlError);
-    expect(mockStderr).toHaveBeenCalledWith('Error: NORMAL_ERR');
+    handleError(errors.normlError);
+    expect(mockFns.mockStderr).toHaveBeenCalledWith('Error: NORMAL_ERR');
   });
 });
--- task2-testing/test/streams.test.js
+++ task2-testing/test/streams.test.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const { expect } = require('@jest/globals');
-const { Readable, Transform, PassThrough } = require('stream');
+const { Readable, Transform } = require('stream');
 const {
   getReadable,
   getWriteable,
@@ -8,6 +8,9 @@ const {
 } = require('../modules/streams');
 
 describe('Stream implementation tests', () => {
+  const streams = {
+    mockReadable: new Readable({ read() {} }),
+  };
   test('Test if readable stream is instance of ReadStream if input file option was passed', () => {
     expect(getReadable('./input.txt') instanceof fs.ReadStream).toEqual(true);
   });
@@ -31,13 +34,17 @@ describe('Stream implementation tests', () => {
   });
 
   test('Test if transform stream encode/decode strings', () => {
-    const mockReadable = new Readable({ read() {} });
     const transform = getTransform('C0');
     let output;
 
-    transform.on('data', (data) => (output = data));
-    mockReadable.pipe(transform);
-    mockReadable.emit('data', 'This is secret. Message about "_" symbol!');
+    transform.on('data', (data) => {
+      output = data;
+    });
+    streams.mockReadable.pipe(transform);
+    streams.mockReadable.emit(
+      'data',
+      'This is secret. Message about "_" symbol!'
+    );
 
     expect(output).toEqual('Sghr hr rdbqds. Ldrrzfd zants "_" rxlank!');
   });
